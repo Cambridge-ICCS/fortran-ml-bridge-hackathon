d9f9d159b28c47135af57366084b29f41e728b14
Valentin Churavy <vchuravy@users.noreply.github.com>
2022-09-23 16:44:54 +0100

    almost

--- fortran-tf-lib/tools/tests/test_process_model_link.py
+++ fortran-tf-lib/tools/tests/test_process_model_link.py
@@ -11,16 +11,16 @@ def my_model(tf_lib):
 	subprocess.run(["process_model", "../my_model/", "-o", "testf.f90"])
 	subprocess.run(["gfortran", "-c", "-I", "../src/", "testf.f90"], check=True)
 
-def test_link(my_model, fc='gfortran', file='test'):
+def test_link(my_model, fc='gfortran'):
     """ 
     Test to check that generated FORTRAN object can be linked with the TF-fortran library
     """ 
     # May be specfic to my system
     # subprocess.run(["export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.local/lib"], shell=True)
-    ret = subprocess.run([fc, "-c", "-I", "../src", f'{file}_script.F90'])
+    ret = subprocess.run([fc, "-c", "-I", "../src", f'test_script.F90'])
     assert ret.returncode == 0
-    ret = subprocess.run([fc, "-I", "../src/", f"{file}_script.o", f"{file}.o",\
+    ret = subprocess.run([fc, "-I", "../src/", f"test_script.o", f"testf.o",\
         "../src/libfortran_tensorflow.a", \
-        "-ltensorflow", "-o", f"{file}_script"])
+        "-ltensorflow", "-o", f"test_script"])
     assert ret.returncode == 0
-    os.remove(f"{file}_script.o")
+    os.remove(f"test_script.o")
